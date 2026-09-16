f26e365a967c9ab069886af80958820691a702ff
SpiritKyu <tgv22362236@gmail.com>
2026-09-16 12:26:29 -0300

    Add files via upload

--- InterfacesWeb/AtividadeAvaliativaIW/js/script.js
+++ InterfacesWeb/AtividadeAvaliativaIW/js/script.js
@@ -7,21 +7,25 @@ const IMAGENS = [
     "carossel/pousada6.png"
 ];
 
-let atual = 0;
+//isso aq é pro site saber quais imagens vai aparecer no carrosel, se é constante é em maiusculo hehe >:)
 
-const IMAGEM = document.getElementById("imagem");
+var atual = 0;
+
+//contagem ne rs, tem q usar oq nois aprendeu na aula passada de pa(em resumo controla qual imagem vai ser exibida)
+
+const IMAGEM = document.getElementById("imagemCarrosel"); //aq busca no html o elemento com o id "imagem" ai o js muda o src dessa imagem 
 
 function atualizarImagem() {
     if (!IMAGEM) return;
     IMAGEM.src = IMAGENS[atual];
-}
+} // essa função verifica se a imagem existe, ai se existir ela troca o atributo src da tag img pra imagem do indice atual
 
 function proxima() {
     atual = (atual + 1) % IMAGENS.length;
     atualizarImagem();
-}
+} //essa avanca pra proxima imagem(% IMAGENS.length faz o ciclo voltar ao inicio quando chega ao fim) ai dps chama o atualizarImagem q troca a imagem do carossel
 
 function anterior() {
     atual = (atual - 1 + IMAGENS.length) % IMAGENS.length;
     atualizarImagem();
-}
+} // mema coisa so q volta pra tras(por isso o -1) ai deixa o carrossel em loop sem quebrar
